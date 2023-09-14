a1b066b9e855b9d0eaf1f9db1757c1fe62574e25
ItsJeff510 <jovannyr72@gmail.com>
2023-09-14 02:20:37 -0700

    Visual bugs on Template page fixed and finished Template for About page w/Read me instructions

--- application/Front-End/src/App.js
+++ application/Front-End/src/App.js
@@ -4,6 +4,7 @@ import Index from './pages/Index';
 import About from './pages/About';
 import Template from './pages/Template';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import JovannyRamos from './pages/JovannyRamos';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Index />} />  
           <Route path="/about" element={<About />} />
           <Route path="/template" element={<Template />} />
+          <Route path="/jovannyramos" element={<JovannyRamos />} />
          </Routes>
         </Router>
     </div>
