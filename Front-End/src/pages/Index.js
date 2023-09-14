c0afe14f7cae3b0f623ee522705fec9d7aee18e0
ItsJeff510 <jovannyr72@gmail.com>
2023-09-13 23:56:22 -0700

    Created React Environment with Pages using Routers

--- Front-End/src/pages/Index.js
+++ Front-End/src/pages/Index.js
@@ -6,7 +6,7 @@ function Index() {
         <Fragment>
             <NavMenu /> 
             <div>
-                <p>sup</p> 
+                <p>Home Page goes here, Go to About page</p> 
             </div>
         </Fragment>
     )
