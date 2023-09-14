80009870deef64ac3b8529165d657af7bfb78697
spushkin <pushkin.s89@gmail.com>
2023-09-14 14:10:42 -0700

    Revert "Added photo to about page"
    This reverts commit 492ca7f08bb9f64acfc95d13bcf9c9ff6ca7f4ab.

--- application/Front-End/src/App.js
+++ application/Front-End/src/App.js
@@ -1,24 +1,22 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Index from "./pages/Index";
-import About from "./pages/About";
-import Template from "./pages/Template";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import JovannyRamos from "./pages/JovannyRamos";
-import SiarheiPushkin from "./pages/SiarheiPushkin";
+import logo from './logo.svg';
+import './App.css';
+import Index from './pages/Index';
+import About from './pages/About';
+import Template from './pages/Template';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import JovannyRamos from './pages/JovannyRamos';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/" element={<Index />} />  
           <Route path="/about" element={<About />} />
           <Route path="/template" element={<Template />} />
           <Route path="/jovannyramos" element={<JovannyRamos />} />
-          <Route path="/siarheipushkin" element={<SiarheiPushkin />} />
-        </Routes>
-      </Router>
+         </Routes>
+        </Router>
     </div>
   );
 }
