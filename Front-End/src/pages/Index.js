57b9462ef924686dcd82b044a06153799932edf2
ItsJeff510 <jovannyr72@gmail.com>
2023-09-13 18:39:49 -0700

    Added React Application and Pages

---
+++ Front-End/src/pages/Index.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import NavMenu from './NavMenu';
+
+function Index() {
+    return (
+        <Fragment>
+            <NavMenu /> 
+            <div>
+                <p>sup</p> 
+            </div>
+        </Fragment>
+    )
+}
+
+export default Index;
