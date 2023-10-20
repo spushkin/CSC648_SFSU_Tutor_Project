56c96e9b6f8070565be9ce62587f134563e2f6a4
Esau4119 <esau4119@gmail.com>
2023-10-20 16:35:42 -0700

    Recently Added - Esau

--- application/Front-End/src/pages/Index.js
+++ application/Front-End/src/pages/Index.js
@@ -17,8 +17,52 @@ function Index() {
                 <div className="bottomTitleBox">
                     <h4 className="center"><i>One of the greatest collection of San Francisco State Universtiy experts in 100+ subjects ready to educate you on your time!</i></h4>
                 </div>
-
-                <div className="tempname"></div>
+                
+                <div className="RecentlyApplied">
+                <h2 className="recent" ><i>Recently Applied</i></h2>
+                    <img src={require('../images/sfLibrary.png')} alt="SFSU Library" className="libraryPic" />
+                    <div className="ContainterForRecents">
+                        <div  className="CardStyle">
+                        <img src={require('../images/sillouette.jpg')} alt="EMPTY" className="proPic"  />
+                            <div className='cardText'>
+                                <h2 ><i>Full Name</i></h2>
+                                <h3 ><i>Main Subject Proficiency</i></h3>
+                                <i>Summary of Tutors Description</i>
+                                <br></br>
+                                <div className='buttonHold'>   
+                                    <button className='cardbutton'>Contact</button>
+                                    <button className='cardbutton'>View</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div  className="CardStyle">
+                        <img src={require('../images/sillouette.jpg')} alt="EMPTY" className="proPic"  />
+                            <div className='cardText'>
+                                <h2 ><i>Full Name</i></h2>
+                                <h3 ><i>Main Subject Proficiency</i></h3>
+                                <i>Summary of Tutors Description</i>
+                                <br></br>
+                                <div className='buttonHold'>   
+                                    <button className='cardbutton'>Contact</button>
+                                    <button className='cardbutton'>View</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div  className="CardStyle">
+                        <img src={require('../images/sillouette.jpg')} alt="EMPTY" className="proPic"  />
+                            <div className='cardText'>
+                                <h2 ><i>Full Name</i></h2>
+                                <h3 ><i>Main Subject Proficiency</i></h3>
+                                <i>Summary of Tutors Description</i>
+                                <br></br>
+                                <div className='buttonHold'>   
+                                    <button className='cardbutton'>Contact</button>
+                                    <button className='cardbutton'>View</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </Fragment>
     )
