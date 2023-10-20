ef240efbdfc86e2f2cf0ea03987ce3b2479f3a2f
spushkin <pushkin.s89@gmail.com>
2023-10-20 02:50:25 -0700

    Added Tutors.js and tutorsStyle.css pages, added footer element, updated link to Tutors page

---
+++ application/Front-End/src/pages/Footer.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+
+function Footer() {
+	return (
+		<Fragment>
+			<footer>Footer</footer>
+		</Fragment>
+	);
+}
+
+export default Footer;
