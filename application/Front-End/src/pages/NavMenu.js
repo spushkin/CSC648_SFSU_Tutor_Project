bf5981154fd35364e7b24f9807de188ffd7e1c2b
ItsJeff510 <jovannyr72@gmail.com>
2023-10-19 19:45:23 -0700

    Adding new implemented navBar by William

--- application/Front-End/src/pages/NavMenu.js
+++ application/Front-End/src/pages/NavMenu.js
@@ -2,16 +2,72 @@ import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 
 function NavMenu() {
-    return(
+    return (
         <Fragment>
-            <div className="nav_links">
-                <Link to="/"><h2 className="logo"><b><i>Cheep</i></b></h2></Link>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                padding: '15px 30px',
+                borderBottom: '1px solid #e0e0e0',
+                backgroundColor: '#e0f7fa'
+            }}>
 
-                <div className="menu">
-                    <Link to="/"><b><i>Become a tutor</i></b></Link>
-                    <Link to="/about"><b><i>About Us</i></b></Link>
-                    <Link to="/"><b><i>Tutors</i></b></Link>
+                {/* Title */}
+                <Link to="/" style={{
+                    fontSize: '30px',
+                    fontWeight: 'bold',
+                    fontStyle: 'italic',
+                    textDecoration: 'none',
+                    color: 'black'
+                }}>Cheep</Link>
+
+
+                {/* Right container */}
+                <div style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    gap: '30px'
+                }}>
+
+                    {/* Navigation Links */}
+                    <div style={{
+                        display: 'flex',
+                        gap: '30px'
+                    }}>
+                        <Link to="/" style={{
+                            fontSize: '18px',
+                            fontWeight: 'bold',
+                            fontStyle: 'italic',
+                            textDecoration: 'none',
+                            color: 'black'
+                        }}>Become a Tutor</Link>
+                        <Link to="/about" style={{
+                            fontSize: '18px',
+                            fontWeight: 'bold',
+                            fontStyle: 'italic',
+                            textDecoration: 'none',
+                            color: 'black'
+                        }}>About Us</Link>
+                        <Link to="/" style={{
+                            fontSize: '18px',
+                            fontWeight: 'bold',
+                            fontStyle: 'italic',
+                            textDecoration: 'none',
+                            color: 'black'
+                        }}>Tutors</Link>
+                    </div>
+
+                    {/* Icon */}
+                    <div style={{
+                        width: '30px',
+                        height: '30px',
+                        borderRadius: '50%',
+                        backgroundColor: 'gray'
+                    }}></div>
+
                 </div>
+
             </div>
         </Fragment>
     )
