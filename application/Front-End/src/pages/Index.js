75e292f15615913fb3380598d60c4bda5dd52917
spushkin <pushkin.s89@gmail.com>
2023-10-23 01:25:50 -0700

    fix search, fix style

--- application/Front-End/src/pages/Index.js
+++ application/Front-End/src/pages/Index.js
@@ -1,43 +1,59 @@
-import React, { Fragment, useState } from 'react';
-import NavMenu from './NavMenu';
-import SearchBar from './SearchBar';
-import TutorCard from './TutorCard';
+import React, { Fragment, useState } from "react";
+import NavMenu from "./NavMenu";
+import SearchBar from "./SearchBar";
+import TutorCard from "./TutorCard";
 import SearchResultsList from "./SearchResultsList";
+import TutorsSearchBox from "./TutorsSearchBox";
+
 function Index() {
-    const [results, setResults] = useState([]);
+	const [results, setResults] = useState([]);
+	return (
+		<Fragment>
+			<NavMenu />
+			<div className="searchBox">
+				<div className="titleBox">
+					<h2 className="center">
+						<i>Best Place to find SFSU tutors</i>
+					</h2>
+					<h2 className="center">
+						<i>for SFSU Students</i>
+					</h2>
+				</div>
 
-    return (
-        <Fragment>
-            <NavMenu /> 
-            <div className="searchBox">
-                <div className="titleBox">
-                    <h2 className="center"><i>Best Place to find SFSU tutors</i></h2>
-                    <h2 className="center"><i>for SFSU Students</i></h2>
-                </div>
+				<SearchBar setResults={setResults} />
 
-                <SearchBar setResults={setResults}/>
-                
-              
-                <div className="bottomTitleBox">
-                    <h4 className="center"><i>One of the greatest collection of San Francisco State Universtiy experts in 100+ subjects ready to educate you on your time!</i></h4>
-                </div>
-                
-                <div className="RecentlyApplied">
-                <h2 className="recent" ><i>Recently Applied Tutors</i></h2>
-                    <img src={require('../images/sfLibrary.png')} alt="SFSU Library" className="libraryPic" />
-                    <div className="ContainterForRecents">
-                        
-                        {/* from: 1 will send results to Tutor card*/}
-                     <SearchResultsList results={results} from={1}/>
-                        <TutorCard/>
-                        <TutorCard/>
-                        <TutorCard/>
-                        <TutorCard/>
-                    </div>
-                </div>
-            </div>
-        </Fragment>
-    )
+				{results.length > 0 && <TutorsSearchBox results={results} />}
+
+				<div className="bottomTitleBox">
+					<h4 className="center">
+						<i>
+							One of the greatest collection of San Francisco State Universtiy
+							experts in 100+ subjects ready to educate you on your time!
+						</i>
+					</h4>
+				</div>
+
+				<div className="RecentlyApplied">
+					<h2 className="recent">
+						<i>Recently Applied Tutors</i>
+					</h2>
+					<img
+						src={require("../images/sfLibrary.png")}
+						alt="SFSU Library"
+						className="libraryPic"
+					/>
+					<div className="ContainterForRecents">
+						{/* from: 1 will send results to Tutor card*/}
+						{/* <SearchResultsList results={results} from={1} /> */}
+						<TutorCard />
+						<TutorCard />
+						<TutorCard />
+						<TutorCard />
+					</div>
+				</div>
+			</div>
+		</Fragment>
+	);
 }
 
 export default Index;
---
+++ application/Front-End/src/pages/TutorCard.jsx
@@ -0,0 +1,85 @@
+import React, { Fragment } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Template from "./Template";
+function TutorCard({ result }) {
+	console.debug(result);
+	let navigate = useNavigate();
+	const routeChange = () => {
+		let path = `/template`;
+		navigate(path);
+	};
+	//This is allowing for place holder cards in Index.js.
+	if (result == null) {
+		return (
+			<Fragment>
+				<div className="CardStyle">
+					<div className="card-left">
+						<img
+							src={require("../images/sillouette.jpg")}
+							alt="EMPTY"
+							className="proPic"
+						/>
+					</div>
+					<div className="card-right">
+						<div className="cardText">
+							<h2>
+								<i>Full Name</i>
+							</h2>
+							<h3>
+								<i>Profession</i>
+							</h3>
+							<i>Description</i>
+							<br></br>
+						</div>
+						<div className="buttonHold">
+							<button className="cardbutton" onClick={routeChange}>
+								Contact
+							</button>
+							<button className="cardbutton" onClick={routeChange}>
+								View
+							</button>
+						</div>
+					</div>
+				</div>
+			</Fragment>
+		);
+	}
+
+	// Used when We have search results in Index.js
+	return (
+		<Fragment>
+			<div className="CardStyle">
+				<div className="card-left">
+					<img
+						src={require("../images/sillouette.jpg")}
+						alt="EMPTY"
+						className="proPic"
+					/>
+				</div>
+				<div className="card-right">
+					<div className="cardText">
+						<h2>
+							<i>{result.name}e</i>
+						</h2>
+						<h3>
+							<i>{result.email}</i>
+						</h3>
+						<i>
+							{result.address.street}
+							{result.address.zipcode}
+						</i>
+						<br></br>
+					</div>
+					<div className="buttonHold">
+						<button className="cardbutton" onClick={routeChange}>
+							Contact
+						</button>
+						<button className="cardbutton">View</button>
+					</div>
+				</div>
+			</div>
+		</Fragment>
+	);
+}
+
+export default TutorCard;
