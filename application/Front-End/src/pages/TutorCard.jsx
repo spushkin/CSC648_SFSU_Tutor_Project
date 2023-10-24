630e0cef17dc77886b8dd4b94d080d0b3bbb984c
ItsJeff510 <jovannyr72@gmail.com>
2023-10-23 17:02:13 -0700

    Photos displaying as needed

--- application/Front-End/src/pages/TutorCard.jsx
+++ application/Front-End/src/pages/TutorCard.jsx
@@ -1,6 +1,6 @@
 import React, { Fragment, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import Template from "./Template";
+
 function TutorCard({ result }) {
 	console.debug(result);
 	let navigate = useNavigate();
