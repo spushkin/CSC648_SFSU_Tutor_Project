5afd7c4818651846e71439bbae682e6a83f7e172
spushkin <pushkin.s89@gmail.com>
2023-11-14 23:46:41 -0800

    comments added

--- application/Front-End/src/pages/FileDropZone.js
+++ application/Front-End/src/pages/FileDropZone.js
@@ -1,36 +1,48 @@
-// drag n drop element for Tutor application page
+/**
+ * Component: FileDropZone
+ * Description: Drag-and-drop element for the Tutor application page.
+ */
 
 import React, { useState } from "react";
 
 const FileDropZone = () => {
+	// state to track dragging state
 	const [dragging, setDragging] = useState(false);
+	// state to store uploaded files
 	const [uploadedFiles, setUploadedFiles] = useState([]);
 
+	// event handler for drag enter
 	const handleDragEnter = (e) => {
 		e.preventDefault();
 		setDragging(true);
 	};
 
+	// event handler for drag leave
 	const handleDragLeave = () => {
 		setDragging(false);
 	};
 
+	// event handler for drag over
 	const handleDragOver = (e) => {
 		e.preventDefault();
 	};
 
+	// event handler for drop
 	const handleDrop = (e) => {
 		e.preventDefault();
 		setDragging(false);
 
+		// get the dropped files
 		const files = Array.from(e.dataTransfer.files);
 
-		// display the uploaded files
+		// update state with the uploaded files
 		setUploadedFiles(files);
+
 		// display logs
 		console.log(files);
 	};
 
+	// JSX structure for the FileDropZone component
 	return (
 		<div
 			className={`file-drop-zone ${dragging ? "dragging" : ""}`}
@@ -41,6 +53,7 @@ const FileDropZone = () => {
 		>
 			<p>Drag and drop your files here</p>
 
+			{/* Display uploaded files if any */}
 			{uploadedFiles.length > 0 && (
 				<div>
 					<p>Uploaded Files:</p>
