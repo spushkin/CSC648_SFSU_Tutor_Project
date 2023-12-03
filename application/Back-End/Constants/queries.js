00457e8e4a5215b27533c05d4243c39d5c6932b5
Akshar <akshar.gothi@tatvasoft.com>
2023-12-02 23:57:57 -0800

    Develop the apis for tutor and user related

--- application/Back-End/Constants/queries.js
+++ application/Back-End/Constants/queries.js
@@ -10,7 +10,15 @@ const queries = {
     searchTutorViaTextAll : `SELECT * 
     FROM Tutor WHERE
     (name LIKE ? OR description LIKE ?)
-    ORDER BY CASE WHEN name LIKE ? THEN 1 ELSE 2 END;`
+    ORDER BY CASE WHEN name LIKE ? THEN 1 ELSE 2 END;`,
+    createUser: `INSERT INTO User (Firstname, Lastname, Username, Email, Password) VALUES(? ,?, ?, ?, ?)`,
+    getUser: `SELECT * FROM User WHERE Email = ?`,
+    createTutor: `INSERT INTO Tutor(Name, Email, TopicId, Cv, Description, Photo, Schedule) VALUES(?,?,?,?,?,?,?)`,
+    getTutor: `SELECT * FROM Tutor WHERE Id= ?`,
+    createMessage: `INSERT INTO Message (SenderId, RecevierId, Text) VALUES(?,?,?)`,
+    getMessage: `SELECT * FROM Message WHERE Id=? GROUP BY RecevierId`,
+    getTopic: `SELECT * FROM Topic`,
+
 };
 
 module.exports = queries;
---
+++ application/Back-End/Services/CreateUser.js
@@ -0,0 +1,18 @@
+
+const queries = require('../Constants/queries.js');
+
+const { runQuery } = require('./DBConnector.js');
+async function createUser(firstname, lastname, username, email, password) {
+    
+    let query = "";
+    let queryResults
+    query = queries.createUser;
+    queryResults = await runQuery(query, [firstname, lastname, username, email, password])
+
+    // return tutors details name email description subjects
+
+    return queryResults;
+
+}
+
+module.exports = createUser;
