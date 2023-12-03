87c48413d1b1098bb340ab489f712c3c87332c58
Akshar <akshar.gothi@tatvasoft.com>
2023-12-02 23:57:57 -0800

    Develop the apis for tutor and user related

--- application/Back-End/APIs/IndexController.js
+++ application/Back-End/APIs/IndexController.js
@@ -1,7 +1,12 @@
 // Import any necessary modules or dependencies
 const searchTutors = require('../Services/SearchTutors');
-
-
+const createUser = require('../Services/CreateUser');
+const getUser = require('../Services/GetUser');
+const createTutor = require('../Services/CreateTutor');
+const getTutor = require('../Services/GetTutor');
+const createMessage = require('../Services/CreateMessage');
+const getMessage = require('../Services/GetMessage');
+const getTopic = require('../Services/GetTopic');
 
 // Define the exported function
 exports.getTutors = async (subject, searchText) => {
@@ -14,6 +19,114 @@ exports.getTutors = async (subject, searchText) => {
 
         // Send the tutors as a response
         return tutors;
+    } catch (error) {
+        // Handle any errors that occur
+        console.error(error);
+      return error;
+    }
+};
+
+// Define the exported function
+exports.createUser = async (firstname, lastname, username, email, password) => {
+    try {
+        // Retrieve all tutors from the database
+        let user;
+        user = await createUser(firstname, lastname, username, email, password);
+
+        // Send the tutors as a response
+        return user;
+    } catch (error) {
+        // Handle any errors that occur
+        console.error(error);
+      return error;
+    }
+};
+
+// Define the exported function
+exports.getUser = async (email, password) => {
+    try {
+        // Retrieve all tutors from the database
+        let user;
+        user = await getUser(email);
+        if(user.password == password){
+            return true;
+        }else{
+            return false
+        }
+    } catch (error) {
+        // Handle any errors that occur
+        console.error(error);
+      return error;
+    }
+};
+
+
+// Define the exported function
+exports.createTutor = async (name, email, topicId, cv, description, photo, schedule) => {
+    try {
+        // Retrieve all tutors from the database
+        let user;
+        user = await createTutor(name, email, topicId, cv, description, photo, schedule);
+        return user;
+    } catch (error) {
+        // Handle any errors that occur
+        console.error(error);
+      return error;
+    }
+};
+
+
+// Define the exported function
+exports.getTutor = async (id) => {
+    try {
+        // Retrieve all tutors from the database
+        let user;
+        user = await getTutor(id);
+
+        return user;
+    } catch (error) {
+        // Handle any errors that occur
+        console.error(error);
+      return error;
+    }
+};
+
+// Define the exported function
+exports.createMessage = async (SenderId, RecevierId, Text) => {
+    try {
+        // Retrieve all tutors from the database
+        let user;
+        user = await createMessage(SenderId, RecevierId, Text);
+
+        return user;
+    } catch (error) {
+        // Handle any errors that occur
+        console.error(error);
+      return error;
+    }
+};
+// Define the exported function
+exports.getMessage = async (id) => {
+    try {
+        // Retrieve all tutors from the database
+        let user;
+        user = await getMessage(id);
+
+        return user;
+    } catch (error) {
+        // Handle any errors that occur
+        console.error(error);
+      return error;
+    }
+};
+// Define the exported function
+exports.getTopic = async () => {
+    try {
+        // Retrieve all tutors from the database
+        let user;
+        user = await getTopic();
+
+        return user;
     } catch (error) {
         // Handle any errors that occur
         console.error(error);
--- application/Back-End/app.js
+++ application/Back-End/app.js
@@ -19,6 +19,42 @@ app.post('/getTutors', bodyParser.json(), async (req, res) => {
     const results = await indexController.getTutors(subject, searchText);
     res.send(results);
 });
+app.post('/createUser', bodyParser.json(), async (req, res) => {
+    const { firstname, lastname, username, email, password} = req.body;
+    console.log("Ffdf");
+    console.log(firstname);
+    const results = await indexController.createUser(firstname, lastname, username, email, password);
+    res.send(results);
+});
+app.post('/getUser', bodyParser.json(), async (req, res) => {
+    const { email, password } = req.body;
+    const results = await indexController.getUser(email, password);
+    res.send(results);
+});
+app.post('/createTutor', bodyParser.json(), async (req, res) => {
+    const { name, email, topicId, cv, description, photo, schedule } = req.body;
+    const results = await indexController.createTutor(name, email, topicId, cv, description, photo, schedule);
+    res.send(results);
+});
+app.post('/getTutor', bodyParser.json(), async (req, res) => {
+    const { id } = req.body;
+    const results = await indexController.getTutor(id);
+    res.send(results);
+});
+app.post('/createMessage', bodyParser.json(), async (req, res) => {
+    const { SenderId, RecevierId, Text } = req.body;
+    const results = await indexController.getTutors(SenderId, RecevierId, Text);
+    res.send(results);
+});
+app.post('/getMessage', bodyParser.json(), async (req, res) => {
+    const { id } = req.body;
+    const results = await indexController.getTutors(id);
+    res.send(results);
+});
+app.post('/getTopic', bodyParser.json(), async (req, res) => {
+    const results = await indexController.getTutors();
+    res.send(results);
+});
 
 app.get('/test', async (req, res) => {
     res.send('Hello World!');
