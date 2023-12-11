f326cbcdaa6dbca8c2950598d987efb4fb075952
Esau4119 <esau4119@gmail.com>
2023-12-10 23:50:37 -0800

    Temp Usage of Tutor Info

--- application/Front-End/src/pages/TutorInfo.js
+++ application/Front-End/src/pages/TutorInfo.js
@@ -1,8 +1,12 @@
 import React, { Fragment } from 'react';
 import NavMenu from './NavMenu';
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate,useLocation } from "react-router-dom";
 
-function TutorInfo() {
+function TutorInfo({  }) {
+// Read values passed on state
+const result = useLocation();
+    console.log(result.state)
+    
     let navigate = useNavigate();
 	const routeChange = () => {
 		let path = `/`;
@@ -14,21 +18,21 @@ function TutorInfo() {
             <div id="outer-container2">
           <div class="infoRow">
                         <div class="infoColumn" >
-                        <img src={require('../images/sillouette.jpg')} alt="YOUR NAME HERE" className="teamPhotoPage2" />
+                        <img src={result.state.Photo} alt="YOUR NAME HERE" className="teamPhotoPage2" />
                     
                          <button className="gobackButton" onClick={routeChange}>GO BACK </button>
                                   
                         </div>
 
                         <div class="infoColumn">
-                            <h1 id="info">Full Name</h1> 
+                            <h1 id="info">{result.state.Name}</h1> 
                             
                             <h3 id="info">Title</h3>
                             
-                            <h4 id="info">Topics: Math English, Science, etc</h4>
+                            <h4 id="info">Topics: {result.state.email}</h4>
                     
                             <br/>
-                            <i id="info3">   Extra large description </i>
+                            <i id="info3">   {result.state.Description}</i>
                             <br/>
                             
                         </div>
@@ -41,9 +45,9 @@ function TutorInfo() {
             <div className="mediaBox">
 
                     <h1 id="">Media:</h1> 
-                    <div class="mediaRow">
+                    <div className="mediaRow">
                         <div class="mediaColumn" >
-                           FILE 1
+                        FILE 1
                         </div>
                         <div class="mediaColumn" >
                         FILE 2 
