c86c4899ef22f6851b1a2edd2d506a0822b742f0
ItsJeff510 <jovannyr72@gmail.com>
2023-12-13 02:58:31 -0800

    API to send Messages added

--- application/Front-End/src/pages/TutorInfo.js
+++ application/Front-End/src/pages/TutorInfo.js
@@ -15,19 +15,36 @@ const result = useLocation();
         { id: 3, title: 'Media 4', image: placeholderMedia },
     ];
 
-
 	const routeChange = () => {
 		let path = `/`;
 		navigate(path);
 	};
     const messages = () => {
         let sent = document.getElementById("msg").value
+        const api = "http://3.101.225.46:8003/CreateMessage"
+        let recieveID = document.getElementById("RecieverID").value
+        const response = fetch(api, {
+			method: "POST",
+			body: JSON.stringify({
+				SenderId: JSON.parse(sessionStorage.getItem('user'))[0].id,
+                RecevierId: recieveID,
+                Text: sent,
+			}),
+			headers: {
+				"Content-type": "application/json; charset=UTF-8",
+			},
+		}).then((response) => {
+			console.log(response.status);
+		});
+
+        
         console.log("Ready to be send message: " + sent)
         document.getElementById("msg").style.display="none";
         
         document.getElementById("but").style.display="none";
         document.getElementById("reachButton2").innerText = "SENT!";
 	};
+
     return (
         <Fragment>
             <NavMenu />
@@ -54,10 +71,12 @@ const result = useLocation();
                         </div>
                         <div className = "ChatSection">
                             <div id="reachButton2" className="reachButton2">
-                            <button id="but" className="reachButton"onClick={messages} >
+                            <button id="but" className="reachButton" onClick={messages} >
                                         Get in Contact 
                             </button>
                             
+                            <input type="hidden" name="RecieverID" id="RecieverID" value={result.state.id}></input>
+
                                 <textarea id="msg" className="sp" rows="4" cols="33">
                                 Hello Tutor...
                                 </textarea>
