b0c061fe8522e975a42bb4ed5157442d4068a4bb
ItsJeff510 <jovannyr72@gmail.com>
2023-12-12 20:13:21 -0800

    Adding Changed made by Seirhei for merge

--- application/Back-End/APIs/IndexController.js
+++ application/Back-End/APIs/IndexController.js
@@ -1,195 +1,209 @@
 // Import any necessary modules or dependencies
-const searchTutors = require('../Services/SearchTutors');
-const createUser = require('../Services/CreateUser');
-const getUser = require('../Services/GetUser');
-const createTutor = require('../Services/CreateTutor');
-const getTutor = require('../Services/GetTutor');
-const getTutorByEmail = require('../Services/GetTutorbyEmail');
-const createMessage = require('../Services/CreateMessage');
-const getMessage = require('../Services/GetMessage');
-const getTopic = require('../Services/GetTopic');
-const createTopic = require('../Services/CreateTopic');
-const createCategory = require('../Services/CreateCategory');
-const getCategory = require('../Services/GetCategory');
-const bycrypt = require('bcryptjs');
+const searchTutors = require("../Services/SearchTutors");
+const createUser = require("../Services/CreateUser");
+const getUser = require("../Services/GetUser");
+const createTutor = require("../Services/CreateTutor");
+const getTutor = require("../Services/GetTutor");
+const getTutorByEmail = require("../Services/GetTutorbyEmail");
+const createMessage = require("../Services/CreateMessage");
+const getMessage = require("../Services/GetMessage");
+const getTopic = require("../Services/GetTopic");
+const createTopic = require("../Services/CreateTopic");
+const createCategory = require("../Services/CreateCategory");
+const getCategory = require("../Services/GetCategory");
+// const bycrypt = require("bcryptjs");
 
 // Define the exported function
 exports.getTutors = async (subject, searchText) => {
-    try {
-        // Retrieve all tutors from the database
-        let tutors;
+	try {
+		// Retrieve all tutors from the database
+		let tutors;
 
-        tutors = await searchTutors(subject.toLowerCase(), searchText.toLowerCase());
+		tutors = await searchTutors(
+			subject.toLowerCase(),
+			searchText.toLowerCase()
+		);
 
-
-        // Send the tutors as a response
-        return tutors;
-    } catch (error) {
-        // Handle any errors that occur
-        console.error(error);
-        return error;
-    }
+		// Send the tutors as a response
+		return tutors;
+	} catch (error) {
+		// Handle any errors that occur
+		console.error(error);
+		return error;
+	}
 };
 
 // Define the exported function
 exports.createUser = async (firstname, lastname, username, email, password) => {
-    try {
-        // Retrieve all tutors from the database
-        let user;
-        user = await createUser(firstname, lastname, username, email, bycrypt.hashSync(password, 10));
+	try {
+		// Retrieve all tutors from the database
+		let user;
 
-        // Send the tutors as a response
-        return user;
-    } catch (error) {
-        // Handle any errors that occur
-        console.error(error);
-        return error;
-    }
+		user = await createUser(firstname, lastname, email, email, password);
+
+		// Send the tutors as a response
+		return user;
+	} catch (error) {
+		// Handle any errors that occur
+		console.error(error);
+		return error;
+	}
 };
 
 // Define the exported function
 exports.getUser = async (email, password) => {
-    try {
-        // Retrieve all tutors from the database
-        let user;
-        user = await getUser(email);
-        if (bycrypt.compareSync(password, user[0].Password)) {
-            return user;
-        } else {
-            return false;
-        }
-    } catch (error) {
-        // Handle any errors that occur
-        console.error(error);
-        return error;
-    }
+	try {
+		// Retrieve user from the database
+		let user = await getUser(email);
+
+		if (user[0] && user[0].Password === password) {
+			return user[0];
+		} else {
+			return false;
+		}
+	} catch (error) {
+		console.error(error);
+		return error;
+	}
 };
-
-
 
 // Define the exported function
-exports.createTutor = async (name, email, topicId, cv, description, photo, schedule) => {
-    try {
-        // Retrieve all tutors from the database
-        let user;
-        user = await createTutor(name, email, topicId, cv, description, photo, schedule);
-        return user;
-    } catch (error) {
-        // Handle any errors that occur
-        console.error(error);
-        return error;
-    }
+exports.createTutor = async (
+	name,
+	email,
+	topicId,
+	cv,
+	description,
+	photo,
+	schedule
+) => {
+	try {
+		// Retrieve all tutors from the database
+		let user;
+		user = await createTutor(
+			name,
+			email,
+			topicId,
+			cv,
+			description,
+			photo,
+			schedule
+		);
+		return user;
+	} catch (error) {
+		// Handle any errors that occur
+		console.error(error);
+		return error;
+	}
 };
-
 
 // Define the exported function
 exports.getTutor = async (id) => {
-    try {
-        // Retrieve all tutors from the database
-        let user;
-        user = await getTutor(id);
+	try {
+		// Retrieve all tutors from the database
+		let user;
+		user = await getTutor(id);
 
-        return user;
-    } catch (error) {
-        // Handle any errors that occur
-        console.error(error);
-        return error;
-    }
+		return user;
+	} catch (error) {
+		// Handle any errors that occur
+		console.error(error);
+		return error;
+	}
 };
 
 exports.getTutorByEmail = async (email) => {
-    try {
-        // Retrieve all tutors from the database
-        let Tutor;
-        Tutor = await getTutorByEmail(email);
+	try {
+		// Retrieve all tutors from the database
+		let Tutor;
+		Tutor = await getTutorByEmail(email);
 
-        return Tutor;
-    } catch (error) {
-        // Handle any errors that occur
-        console.error(error);
-        return error;
-    }
+		return Tutor;
+	} catch (error) {
+		// Handle any errors that occur
+		console.error(error);
+		return error;
+	}
 };
-
 
 // Define the exported function
 exports.createMessage = async (SenderId, RecevierId, Text) => {
-    try {
-        // Retrieve all tutors from the database
-        let user;
-        user = await createMessage(SenderId, RecevierId, Text);
+	try {
+		// Retrieve all tutors from the database
+		let user;
+		user = await createMessage(SenderId, RecevierId, Text);
 
-        return user;
-    } catch (error) {
-        // Handle any errors that occur
-        console.error(error);
-        return error;
-    }
+		return user;
+	} catch (error) {
+		// Handle any errors that occur
+		console.error(error);
+		return error;
+	}
 };
 // Define the exported function
 exports.getMessage = async (id) => {
-    try {
-        // Retrieve all tutors from the database
-        let user;
-        user = await getMessage(id);
+	try {
+		// Retrieve all tutors from the database
+		let user;
+		user = await getMessage(id);
 
-        return user;
-    } catch (error) {
-        // Handle any errors that occur
-        console.error(error);
-        return error;
-    }
+		return user;
+	} catch (error) {
+		// Handle any errors that occur
+		console.error(error);
+		return error;
+	}
 };
 // Define the exported function
 exports.getTopic = async () => {
-    try {
-        // Retrieve all tutors from the database
-        let user;
-        user = await getTopic();
+	try {
+		// Retrieve all tutors from the database
+		let user;
+		user = await getTopic();
 
-        return user;
-    } catch (error) {
-        // Handle any errors that occur
-        console.error(error);
-        return error;
-    }
+		return user;
+	} catch (error) {
+		// Handle any errors that occur
+		console.error(error);
+		return error;
+	}
 };
 exports.createTopic = async (Name, CategoryID, Description) => {
-    try {
-        // Retrieve all tutors from the database
-        let topic;
-        topic = await createTopic(Name, CategoryID, Description);
+	try {
+		// Retrieve all tutors from the database
+		let topic;
+		topic = await createTopic(Name, CategoryID, Description);
 
-        return topic;
-    } catch (error) {
-        // Handle any errors that occur
-        console.error(error);
-        return error;
-    }
+		return topic;
+	} catch (error) {
+		// Handle any errors that occur
+		console.error(error);
+		return error;
+	}
 };
 exports.createCategory = async (Name, Description) => {
-    try {
-        // Retrieve all tutors from the database
-        let category;
-        category = await createCategory(Name, Description);
+	try {
+		// Retrieve all tutors from the database
+		let category;
+		category = await createCategory(Name, Description);
 
-        return category;
-    } catch (error) {
-        // Handle any errors that occur
-        console.error(error);
-        return error;
-    }
+		return category;
+	} catch (error) {
+		// Handle any errors that occur
+		console.error(error);
+		return error;
+	}
 };
 exports.getCategory = async () => {
-    try {
-        // Retrieve all tutors from the database
-        let categories;
-        categories = await getCategory();
+	try {
+		// Retrieve all tutors from the database
+		let categories;
+		categories = await getCategory();
 
-        return categories;
-    } catch (error) {
-        // Handle any errors that occur
-        console.error(error);
-        return error;
-    }
+		return categories;
+	} catch (error) {
+		// Handle any errors that occur
+		console.error(error);
+		return error;
+	}
 };
